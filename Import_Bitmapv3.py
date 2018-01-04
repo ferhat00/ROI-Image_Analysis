d4d64e2204b59feea450989b6c0184bb8b59a334
ferhat00 <30912225+ferhat00@users.noreply.github.com>
2018-01-04 10:33:51 +0000

    Add files via upload
    Save the image files

--- Import_Bitmapv3.py
+++ Import_Bitmapv3.py
@@ -19,7 +19,7 @@ import numpy as np
 from PIL import Image
 
 #input your local path here
-path = '//Sstnas002/opgprojects/554 - RemDB/Technical/MAIT/TestResults/20171020 - TDS Camera T Chamber Light Wall Gain Tests'
+path = '//Sstnas002/opgprojects/554 - RemDB/Technical/MAIT/TestResults/20171108 - TDS Camera FPN check integrating sphere/SL24-5 640x480/Bright'
 
 #create your arrays
 Mean_Total = []
@@ -31,10 +31,10 @@ Number_Total = []
 bmp = []
 
 #input ROI here in pixels
-minr = 550
-minc = 800 
-maxr = 680
-maxc = 930
+minr = 350 #min row number
+minc = 500 #min column number
+maxr = 450 #max row number
+maxc = 600 ##max column number
 
 #A for loop to process all the image files in your local folder
 files = [f for f in os.listdir(path) if os.path.splitext(f)[-1] == '.bmp']
@@ -71,18 +71,25 @@ for file in files:
     SD_Total = np.append(SD_Total, SD)
     Min_Total = np.append(Min_Total, Min)
     Max_Total = np.append(Max_Total, Max)
-    number = float(file[13:16])
+    number = float(file[3:6]) #This is where you index the file name number with setting of interest
     Number_Total = np.append(Number_Total, number)
 
 # Plot the mean and standard deviation within the ROI for each image    
 plt.figure(2)
 plt.scatter(Number_Total, Mean_Total, label = 'Data') # Mean vs. Gain
-plt.title('Mean vs Gain')
-plt.xlabel('Gain Setting')
+plt.title('Mean vs FPN')
+plt.xlabel('FPN Setting')
 plt.ylabel('Mean Signal in ROI (ADU)')
+#save the plot
+plt.savefig('//Sstnas002/opgprojects/554 - RemDB/Technical/MAIT/TestResults/20171108 - TDS Camera FPN check integrating sphere/SL24-5 640x480/Bright/Mean.png')
 
 plt.figure(3)
 plt.scatter(Number_Total, SD_Total, label = 'Data') # Standard Deviation vs. Gain
-plt.title('StD vs Gain')
-plt.xlabel('Gain Setting')
+plt.title('StD vs FPN')
+plt.xlabel('FPN Setting')
 plt.ylabel('Standard Deviation of Signal in ROI (ADU)')
+#save the plot
+plt.savefig('//Sstnas002/opgprojects/554 - RemDB/Technical/MAIT/TestResults/20171108 - TDS Camera FPN check integrating sphere/SL24-5 640x480/Bright/SD.png')
+
+#save sample file under analysis
+fig.savefig('//Sstnas002/opgprojects/554 - RemDB/Technical/MAIT/TestResults/20171108 - TDS Camera FPN check integrating sphere/SL24-5 640x480/Bright/Sample_Analysis.png')
